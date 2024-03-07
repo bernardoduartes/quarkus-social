f9a09ef7eae43bdcdb3bd15f4455d1492cd3c33c
Bernardo Duarte <bernardoduartes@gmail.com>
2024-03-07 15:29:20 -0300

    feat: a mock POST users

---
+++ src/main/java/br/shizuca/social/resource/UserResource.java
@@ -0,0 +1,20 @@
+package br.shizuca.social.resource;
+
+import br.shizuca.social.dto.CreateUserRequest;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    public Response create(CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+}
