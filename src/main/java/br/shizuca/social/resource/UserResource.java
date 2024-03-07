fa50651b39f7386eb7c001f24bb8e4455077216b
Bernardo Duarte <bernardoduartes@gmail.com>
2024-03-07 16:32:31 -0300

    feat: POST a User and GET all users

--- src/main/java/br/shizuca/social/resource/UserResource.java
+++ src/main/java/br/shizuca/social/resource/UserResource.java
@@ -1,10 +1,9 @@
 package br.shizuca.social.resource;
 
+import br.shizuca.social.domain.model.User;
 import br.shizuca.social.dto.CreateUserRequest;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -14,7 +13,17 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     @POST
+    @Transactional
     public Response create(CreateUserRequest userRequest) {
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+        user.persist();
         return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response findAll() {
+        return Response.ok(User.listAll()).build();
     }
 }
