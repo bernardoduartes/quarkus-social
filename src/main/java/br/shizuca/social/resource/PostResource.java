ffcba7132b007913e777dcce36849b269ed92175
Bernardo Duarte <bernardoduartes@gmail.com>
2024-03-09 16:52:17 -0300

    feat: get post only from followed users

--- src/main/java/br/shizuca/social/resource/PostResource.java
+++ src/main/java/br/shizuca/social/resource/PostResource.java
@@ -1,7 +1,9 @@
 package br.shizuca.social.resource;
 
+import br.shizuca.social.domain.model.Follower;
 import br.shizuca.social.domain.model.Post;
 import br.shizuca.social.domain.model.User;
+import br.shizuca.social.domain.repository.FollowerRepository;
 import br.shizuca.social.domain.repository.PostRepository;
 import br.shizuca.social.dto.CreatePostRequest;
 import br.shizuca.social.dto.PostResponse;
@@ -13,6 +15,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -23,10 +26,12 @@ public class PostResource {
 
 
     private PostRepository repository;
+    private FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(PostRepository repository) {
+    public PostResource(PostRepository repository, FollowerRepository followerRepository) {
         this.repository = repository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -49,10 +54,31 @@ public class PostResource {
     }
 
     @GET
-    public Response listPosts(@PathParam("userId") Long userId) {
+    public Response listPosts(
+            @PathParam("userId") Long userId,
+            @HeaderParam("followerId") Long followerId) {
+
+        if (Objects.isNull(followerId))
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("HeaderParam followerId is required")
+                    .build();
+
         Optional<User> user = User.findByIdOptional(userId);
         if (!user.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        Optional<User> fallowOptional = User.findByIdOptional(followerId);
+        if (!fallowOptional.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Follow NOT_FOUND")
+                    .build();
+        }
+
+        boolean isFallow = followerRepository.isFollower(fallowOptional.get(), user.get());
+
+        if(!isFallow){
+            return Response.status(Response.Status.FORBIDDEN).entity("You have no acess to the post").build();
         }
 
         var query = repository.find("user", Sort.by("dateTime", Sort.Direction.Descending),user.get());
