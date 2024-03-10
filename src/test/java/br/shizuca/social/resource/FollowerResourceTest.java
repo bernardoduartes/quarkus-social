75cb7b1b8a9bb0d3dbfc4fc099fa326ab6cfb8cc
Bernardo Duarte <bernardoduartes@gmail.com>
2024-03-09 22:01:52 -0300

    test: add test should_return_409_when_follower_id_equals_to_user_id

---
+++ src/test/java/br/shizuca/social/resource/FollowerResourceTest.java
@@ -0,0 +1,51 @@
+package br.shizuca.social.resource;
+
+import br.shizuca.social.domain.model.User;
+import br.shizuca.social.dto.FollowerRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.Response;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(FollowerResource.class)
+class FollowerResourceTest {
+
+    private Long userId;
+
+    @BeforeEach
+    @Transactional
+    void setUp() {
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        User.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("should return 409 when Follower id equals to User id")
+    public void should_return_409_when_follower_id_equals_to_user_id(){
+
+        var body = new FollowerRequest();
+        body.setFollowerId(userId);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .pathParam("userId", userId)
+                .when()
+                .put()
+                .then()
+                .statusCode(Response.Status.CONFLICT.getStatusCode())
+                .body(Matchers.is("You cant fallow youself"));
+    }
+}
